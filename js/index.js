4dfe12ce8a59aa42e2418e05cf142d54d79566a1
n48331 <unfolks@gmail.com>
2022-07-26 21:42:33 +0530

    web design

--- js/index.js
+++ js/index.js
@@ -2,4 +2,25 @@ $(document).ready(function () {
   $(".header a").click(function () {
     $(this).css("color", "red");
   });
+
+  $(function () {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 2,
+      margin: 20,
+      loop: false,
+      nav: true,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+
+        800: {
+          items: 2,
+        },
+      },
+    });
+  });
 });
